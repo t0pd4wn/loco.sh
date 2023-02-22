ec94fb66f1831c0dfa95097ddad9fbcf4106d527
t0pd4wn <t0pd4wn@protonmail.com>
2023-02-22 16:54:19 +0100

    add : yq wrappers (contains, delete, add, delete_key, add_key)

--- src/modules/core/utils.sh
+++ src/modules/core/utils.sh
@@ -535,7 +535,7 @@ utils::yaml_get_keys(){
 }
 
 #######################################
-# Return yaml values
+# Return yaml values (deprecated?)
 # Globals:
 #   PROFILE
 #   PROFILES_DIR
@@ -563,6 +563,32 @@ utils::yaml_get_values(){
 }
 
 #######################################
+# Return yaml values
+# Arguments:
+#   $1 # a yaml selector ".variable.path"
+#   $2 # a yaml file path
+#######################################
+utils::yq(){
+  # local options="${1-}"
+  local selector="${1-}" 
+  local yaml="${2-}"
+
+  # check if selector exist in file
+  utils::yq_has_selector "${selector}" "${yaml}"
+
+  # check if error code is 0
+  if (( $? != 0 )); then
+    # if not, propagates a 1 exit code
+    return 1
+  else
+    # if yes, tries to recover value
+    if ! cat "${yaml}" | yq "${selector}"; then
+      echo "Unable to yq ${selector} in ${yaml}"
+    fi
+  fi
+}
+
+#######################################
 # Check if a yaml selector is present in file
 # Globals:
 #   PROFILE
@@ -571,7 +597,7 @@ utils::yaml_get_values(){
 #   $1 # a yaml selector ".variable.path"
 #   $2 # a yaml file path
 #######################################
-utils::yq_has(){ 
+utils::yq_has_selector(){ 
   local selector="${1-}" 
   local yaml="${2-}"
   # local child_selector=$(utils::echo "${selector}" | grep -oE "[^.]+$")
@@ -597,27 +623,150 @@ utils::yq_has(){
 }
 
 #######################################
-# Return yaml values
+# Return a boolean if value is found
 # Arguments:
-#   $1 # a yaml selector ".variable.path"
-#   $2 # a yaml file path
+#   $1 # a yaml file path
+#   $2 # a yaml selector ".variable.path"
+#   $3 # a yaml value
 #######################################
-utils::yq(){
-  # local options="${1-}"
-  local selector="${1-}" 
-  local yaml="${2-}"
+utils::yq_contains(){
+  local yaml="${1-}"
+  local selector="${2-}" 
+  local value="${3-}"
 
-  # check if selector exist in file
-  utils::yq_has "${selector}" "${yaml}"
-
-  # check if error code is 0
-  if (( $? != 0 )); then
-    # if not, propagates a 1 exit code
-    return 1
+  local yaml_value=$(utils::yq "${selector}" "${yaml}")
+  
+  if [[ "${yaml_value}" == *"${value}"* ]]; then
+    echo true
   else
-    # if yes, tries to recover value
-    if ! cat "${yaml}" | yq "${selector}"; then
-      echo "Unable to yq ${selector} in ${yaml}"
+    echo false
+  fi
+}
+
+#######################################
+# Delete a yaml value
+# Arguments:
+#   $1 # a yaml file path
+#   $2 # a yaml selector ".variable.path"
+#   $3 # a yaml value
+#######################################
+utils::yq_delete(){
+  local yaml="${1-}"
+  local selector="${2-}" 
+  local value="${3-}"
+  
+  local arg="${selector}"'.[] | select(. == "'"${value}"'")'
+
+  # check if value exist
+  local hasValue=$(utils::yq_contains "${yaml}" "${selector}" "${value}" )
+
+  if "${hasValue}"; then
+    # tries to delete value
+    if ! cat "${yaml}" | yq 'del('"${arg}"')' > src/temp/yaml.local; then
+      echo "Unable to yq delete ${selector}[${value}] in ${yaml}"
+    else
+      # if succeeds, overwrites original yaml
+      # condition used to ensure yaml.local is written by yq
+      if [[ -f src/temp/yaml.local ]]; then
+        cat src/temp/yaml.local > "${yaml}"
+      fi
+    fi
+  fi
+}
+
+#######################################
+# Add a yaml value
+# Arguments:
+#   $1 # a yaml file path
+#   $2 # a yaml selector ".variable.path"
+#   $3 # a yaml value
+#######################################
+utils::yq_add(){
+  local yaml="${1-}"
+  local selector="${2-}" 
+  local value="${3-}"
+  
+  local arg="${selector}"' = ["'"${value}"'"] + '"${selector}"
+
+  # check if value exist
+  local hasValue=$(utils::yq_contains "${yaml}" "${selector}" "${value}" )
+
+  if "${hasValue}"; then
+    # value already exist
+    :
+  else
+      # tries to add value
+    if ! cat "${yaml}" | yq "${arg}" > src/temp/yaml.local; then
+      echo "Unable to yq add ${value} in ${selector} in ${yaml}"
+    else
+      # if succeeds, overwrites original yaml
+      # condition used to ensure yaml.local is written by yq
+      if [[ -f src/temp/yaml.local ]]; then
+        cat src/temp/yaml.local > "${yaml}"
+      fi
+    fi
+  fi
+}
+
+#######################################
+# Add a yaml nested key
+# Arguments:
+#   $1 # a yaml file path
+#   $2 # a yaml selector ".parent.path"
+#   $3 # a yaml value ".childpath"
+#######################################
+utils::yq_add_key(){
+  local yaml="${1-}"
+  local selector="${2-}"
+  local value="${3-}"
+  
+  local arg="${selector}""${value}"' += []'
+
+  # tries to add key
+  # check if key exists already
+  utils::yq_has_selector "${selector}""${value}" "${yaml}"
+  if (( $? != 0 )); then
+    # if the key desn't exist, create one
+    if ! cat "${yaml}" | yq "${arg}" > src/temp/yaml.local; then
+      echo "Unable to yq add key ${value} in ${selector} in ${yaml}"
+    else
+      # if succeeds, overwrites original yaml
+      # condition used to ensure yaml.local is written by yq
+      if [[ -f src/temp/yaml.local ]]; then
+        cat src/temp/yaml.local > "${yaml}"
+      fi
+    fi
+  fi
+}
+
+#######################################
+# Delete a yaml nested key
+# Arguments:
+#   $1 # a yaml file path
+#   $2 # a yaml selector ".parent.path"
+#   $3 # a yaml value ".childpath"
+#######################################
+utils::yq_delete_key(){
+  local yaml="${1-}"
+  local selector="${2-}"
+  local value="${3-}"
+  
+  local arg='del('"${selector}""${value}"')'
+
+  # tries to delete key
+  # check if key exists already
+  utils::yq_has_selector "${selector}""${value}" "${yaml}"
+
+  if (( $? == 0 )); then
+    # if the key exists, delete it
+    if ! cat "${yaml}" | yq "${arg}" > src/temp/yaml.local; then
+      echo "Unable to yq delete key ${value} in ${selector} in ${yaml}"
+    else
+      # if succeeds, overwrites original yaml
+      # condition used to ensure yaml.local is written by yq
+      if [[ -f src/temp/yaml.local ]]; then
+        cat src/temp/yaml.local > "${yaml}"
+      fi
     fi
   fi
 }
