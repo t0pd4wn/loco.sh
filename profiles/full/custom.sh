ca462a673eae5904c2baec2275138fcef8cdd939
t0pd4wn <t0pd4wn@protonmail.com>
2023-04-29 15:58:00 +0200

    fix: corrected auto-hide/intellihide values, so dock hides/appears on VMs

--- profiles/full/custom.sh
+++ profiles/full/custom.sh
@@ -66,16 +66,16 @@ remove_ubuntu_exit(){
 # Custom functions
 #################
 set_ubuntu_style(){
-  # set ubuntu 22.04 custom dock style
-  if [[ "${SHORT_OS_VERSION}" == "21" ]] || [[ "${SHORT_OS_VERSION}" == "22" ]] || [[ "${SHORT_OS_VERSION}" == "23" ]]; then
+  # set ubuntu 21.04 and above dock style
+  if [[ "${SHORT_OS_VERSION}" -ge 21 ]]; then
     cmd::record "gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true;"
     cmd::record "gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 5750;"
     cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false;"
     cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM;"
     cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 60;"
-    # cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock autohide false"
+    cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock autohide true"
+    cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true"
     # cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false"
-    # cmd::record "gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false"
   fi
   # set ubuntu folders style
   cmd::record "gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view';"
@@ -84,15 +84,15 @@ set_ubuntu_style(){
 }
 
 unset_ubuntu_style(){
-  # set ubuntu 22.04 default dock style
-  if [[ "${SHORT_OS_VERSION}" == "21" ]] || [[ "${SHORT_OS_VERSION}" == "22" ]] || [[ "${SHORT_OS_VERSION}" == "23" ]]; then
+  # reset ubuntu 21.04 and above default dock style
+  if [[ "${SHORT_OS_VERSION}" -ge 21 ]]; then
     cmd::record "gsettings reset org.gnome.settings-daemon.plugins.color night-light-enabled;"
     cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock extend-height;"
     cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock dock-position;"
     cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock dash-max-icon-size;"
     cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock autohide;"
-    cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock dock-fixed;"
     cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock intellihide;"
+    # cmd::record "gsettings reset org.gnome.shell.extensions.dash-to-dock dock-fixed;"
   fi
   # reset ubuntu folders style
   cmd::record "gsettings reset org.gnome.nautilus.preferences default-folder-viewer;"
