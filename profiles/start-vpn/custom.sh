77991e2e25880c20467e0b070c5313f8f9567f4a
t0pd4wn <t0pd4wn@protonmail.com>
2023-05-13 15:34:48 +0200

    add: added a 'start-vpn' profile and modified .loco_startup to accept configuration files rather than names

---
+++ profiles/start-vpn/custom.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+#-------------------------------------------------------------------------------
+# custom.sh | custom user scripts
+#-------------------------------------------------------------------------------
+
+#################
+# all OS
+#################
+install_exit(){}
+
+remove_exit(){}
+
+#################
+# macOS related
+#################
+install_macos_exit(){}
+
+update_macos_exit(){}
+
+remove_macos_exit(){}
+
+#################
+# ubuntu related
+#################
+install_ubuntu_exit(){}
+
+update_ubuntu_exit(){}
+
+remove_ubuntu_exit(){}
+
+#################
+# Custom functions
+#################
+set_ubuntu_style(){
+}
+
+unset_ubuntu_style(){
+}
+
+set_macos_style(){
+}
+
+unset_macos_style(){
+}
